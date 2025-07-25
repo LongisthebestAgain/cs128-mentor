8161756a4e52a6270943717a6dde274db25fcb49
SongUylong <songuylong24@gmail.com>
2025-07-25 14:54:00 +0700

    changes

---
+++ 25-javascript/script.js
@@ -0,0 +1,21 @@
+const B = 10;
+// console.log(B + 5);
+let C = A + B;
+// console.log(C);
+
+let D = "Hello";
+let E = 2;
+// console.log(D + E);
+
+let text = "ABCDEF";
+// console.log(text.length);
+let char = text.charAt(2);
+// console.log(char);
+
+let text2 = "GH";
+
+// console.log(text + text2);
+
+let text3 = `I'm missing ${text}`;
+
+console.log(text3);
